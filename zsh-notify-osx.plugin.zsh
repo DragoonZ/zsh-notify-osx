42d6d1d222bda1b05714dc647834720330c187b9
Len Zhang <zyldragoon@gmail.com>
2016-04-05 11:29:53 -0400

    add group id to remove previously notification

--- zsh-notify-osx.plugin.zsh
+++ zsh-notify-osx.plugin.zsh
@@ -22,9 +22,11 @@ function notifyosd-precmd() {
         if [ $retval -gt 0 ]; then
 			cmdstat="with warning"
 			sndstat="Funk"
+            groupstat="cmd success"
 		else
             cmdstat="successfully"
 			sndstat="Glass"
+            groupstat="cmd fail"
         fi
         if [ ! -z "$cmd" -a $cmd_secs -gt 10 ]; then
 			if [ $gnuunits -gt 0 ]; then
@@ -34,9 +36,9 @@ function notifyosd-precmd() {
 				cmd_time="$cmd_secs seconds"
 			fi
             if [ ! -z $SSH_TTY ] ; then
-								terminal-notifier -message "$cmd took $cmd_time" -title "$cmd_basename on `hostname` completed $cmdstat" -activate "$term_id" -sender "$term_id" -sound $sndstat
+								terminal-notifier -message "$cmd took $cmd_time" -title "$cmd_basename on `hostname` completed $cmdstat" -activate "$term_id" -group $groupstat -sender "$term_id" -sound $sndstat
             else
-								terminal-notifier -message "$cmd took $cmd_time" -title "$cmd_basename completed $cmdstat" -activate "$term_id" -sender "$term_id" -sound $sndstat
+								terminal-notifier -message "$cmd took $cmd_time" -title "$cmd_basename completed $cmdstat" -activate "$term_id" -group $groupstat -sender "$term_id" -sound $sndstat
             fi
         fi
         unset cmd
